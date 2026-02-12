45e770db55fada6920f288d4744bbc4357454a1c
Victoria Morais <victoriamoraisnobre14@gmail.com>
2026-02-12 12:26:29 -0300

    update

---
+++ sc.js
@@ -0,0 +1,27 @@
+// RPC 
+
+function play(user) {
+    const op = ["⚫", "📄", "✂️"];
+
+    const cp = op[Math.floor(Math.random() * 3)];
+
+    let rs = "";
+
+    if (user === cp){
+        rs = "DRAW!";
+    } else if (
+        (user === "⚫" && cp === "✂️") || 
+        (user === "📄" && cp === "⚫") ||
+        (user === "✂️" && cp === "📄")
+    ) {
+        rs = "You win! 🎉"
+    } else {
+        rs = "You lost! 😔"
+    }
+
+    document.getElementById('result').innerText =
+    ` Your choice: ${user}
+      Computer coice: ${cp}
+      ${rs}`;
+
+}
